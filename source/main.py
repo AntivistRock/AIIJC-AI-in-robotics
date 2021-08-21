eeec79dea0337eb6c3c0218338196de118495d49
prExidna <dm.a.titov@ya.ru>
2021-08-22 01:16:10 +0300

    zero point

---
+++ source/main.py
@@ -0,0 +1,21 @@
+import pybullet as pb
+import pybullet_data
+
+import time
+
+
+def main():
+    phys_client = pb.connect(pb.GUI)
+    pb.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    pb.loadURDF("r2d2.urdf")
+
+    for i in range(10000):
+        pb.stepSimulation()
+        time.sleep(1./240.)
+
+    pb.disconnect(phys_client)
+
+
+if __name__ == "__main__":
+    main()
