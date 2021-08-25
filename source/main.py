ab6f94f50d2869368116ffa3edef078b4d5ea87e
Титов Дмитрий <73162077+prExidna@users.noreply.github.com>
2021-08-25 21:53:50 +0300

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,47 +1,7 @@
-<<<<<<< Updated upstream
-import pybullet as pb
-import pybullet_data
-=======
 from decorators import add_timer
 from engine import Environment, EnvThreadPool
 from my_simulation import MySimulation
 
->>>>>>> Stashed changes
-
-@add_timer
-def test_1():
-    env = Environment(simulation=MySimulation())
-    for i in range(10):
-        env.run()
-
-<<<<<<< Updated upstream
-from source import engine
-
-
-class MySimulation(engine.res.Simulation):
-
-    def _load(self):
-        pb.setGravity(0, 0, -9.8)
-
-        pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-        pb.loadURDF("plane.urdf")
-
-        vm_data = engine.ViewMatrix.ViewMatrixData([0, 0, 5], 10, 2)
-        pm_data = engine.ProjMatrix.ProjMatrixData(0.01, 20)
-
-        self.camera = engine.Camera(200, 200, vm_data, pm_data)
-
-    def _update(self):
-        plt.imshow(self.camera.snapshot())
-
-
-def main():
-    env = engine.Environment()
-    env.set_simulation(MySimulation())
-
-    for i in range(10000):
-        env.update()
-=======
 
 @add_timer
 def test_2():
@@ -52,7 +12,6 @@ def test_2():
 def main():
     test_1()
     test_2()
->>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
