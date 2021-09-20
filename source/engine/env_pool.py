e29e20aa626761fdc4dbbc7cc52ebb96e30d0164
Титов Дмитрий <73162077+prExidna@users.noreply.github.com>
2021-09-20 17:53:11 +0300

    Update env_pool.py

--- source/engine/env_pool.py
+++ source/engine/env_pool.py
@@ -13,12 +13,10 @@ class EnvPool(utils.ThreadPool):
 
         super().__init__(worker)
 
-        self.model = model
-        self.envs = [Environment(None) for _ in range(self._n_parallel)]
+        self.envs = [Environment(deepcopy(model)) for _ in range(self._n_parallel)]
 
     def interract(self, quantity, num_steps):
 
-        [env.set_model(deepcopy(self.model)) for env in self.envs]
         arguments = [[self.envs[i], num_steps] for i in range(self._n_parallel)]
         histories = self.run(quantity, arguments)
 
